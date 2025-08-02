b479328e0f1cf4a14b7e03147aefc8be1bb8c04a
blytheb <builtbyblythe@gmail.com>
2025-08-01 22:06:59 -1000

    added firebase service acct to gitignore. Server works

--- firebase.js
+++ firebase.js
@@ -1,13 +1,15 @@
 import admin from 'firebase-admin';
+import serviceAccount from './firebase-service-account.json' with {type: "json"};
 
 admin.initializeApp({
-    credential: admin.credential.cert(process.env.FIREBASE_SERIVCE_ACCOUNT),
+    credential: admin.credential.cert(serviceAccount),
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_API_ID
+    
 });
 
 export default admin;
