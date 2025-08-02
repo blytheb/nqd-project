b292d66f01aacce4784ff9f4f1043b106f6e8fb4
blytheb <builtbyblythe@gmail.com>
2025-08-01 21:51:30 -1000

    removed file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const admin = require('./config/firebase');
+const admin = require('./firebase');
 
 const app = express();
 
